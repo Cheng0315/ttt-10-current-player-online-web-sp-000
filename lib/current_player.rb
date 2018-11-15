e87eddbd4ac9ca306c6c7a165e1c10f8bf93fd7b
Cheng Vue <cvu3@ucsc.edu>
2018-11-15 20:41:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,13 +6,10 @@ def turn_count(board)
       count += 1
     end
   end
+
   return count
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    "X"
-  else
-    "O"
-  end
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
